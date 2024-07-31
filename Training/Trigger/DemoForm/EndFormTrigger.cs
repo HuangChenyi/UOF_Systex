1c175520b189b5de598024de6da0d7a683ab1b62
woodman <woodman@edetw.com>
2024-07-31 16:00:48 +0800

    A單起B單

--- Training/Trigger/DemoForm/EndFormTrigger.cs
+++ Training/Trigger/DemoForm/EndFormTrigger.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Xml.Linq;
+using Ede.Uof.Utility.Log;
 using Ede.Uof.WKF.ExternalUtility;
 using Training.UCO;
 
@@ -35,6 +36,65 @@ namespace Training.Trigger.DemoForm
             string signStatus = applyTask.FormResult.ToString();
 
             uco.UpdateFormResult(docNbr, signStatus);
+         
+            if(applyTask.FormResult == Ede.Uof.WKF.Engine.ApplyResult.Adopt)
+            {
+                //todo
+                //<Form formVersionId="4dffe3b4-7a4d-400c-9968-bfdd5eac9dee">
+                //  <FormFieldValue>
+                //    <FieldItem fieldId="NO" fieldValue="" realValue="" enableSearch="True" />
+                //    <FieldItem fieldId="A01" ConditionValue="" realValue="" fillerName="Tony" fillerUserGuid="c496e32b-0968-4de5-95fc-acf7e5a561c0" fillerAccount="Tony" fillSiteId="">
+                //      <FormChooseInfo taskGuid="66d203b9-fe6d-4745-afb4-a4babd727f92" />
+                //    </FieldItem>
+                //    <FieldItem fieldId="type" fieldValue="A" realValue="" enableSearch="True" customValue="@null" fillerName="Tony" fillerUserGuid="c496e32b-0968-4de5-95fc-acf7e5a561c0" fillerAccount="Tony" fillSiteId="" />
+                //    <FieldItem fieldId="item" fieldValue="s" realValue="" enableSearch="True" fillerName="Tony" fillerUserGuid="c496e32b-0968-4de5-95fc-acf7e5a561c0" fillerAccount="Tony" fillSiteId="" />
+                //    <FieldItem fieldId="amount" fieldValue="2" realValue="" enableSearch="True" fillerName="Tony" fillerUserGuid="c496e32b-0968-4de5-95fc-acf7e5a561c0" fillerAccount="Tony" fillSiteId="" />
+                //  </FormFieldValue>
+                //</Form>
+                var applyInfo = applyTask.Task.Applicant;
+                LabForm form = new LabForm("4dffe3b4-7a4d-400c-9968-bfdd5eac9dee",
+                     UrgentLevel.Normal,
+                     applyInfo.Account,applyInfo.UserGUID,
+                     applyInfo.UserName);
+
+
+                form.Fields.Field_type.FieldValue = "A";
+                form.Fields.Field_NO.IsNeedAutoNbr = true.ToString();
+                form.Fields.Field_item.FieldValue = "BB";
+                form.Fields.Field_amount.FieldValue = "100";
+                form.Fields.Field_NO.FieldValue = DateTime.Now.ToString("yyyyMMddHHmmss");
+                XElement a01Value = new XElement("FormChooseInfo",
+                    new XAttribute("taskGuid",applyTask.TaskId));
+                form.Fields.Field_A01.FieldValue = a01Value.ToString();
+
+                string result = "";
+
+                Ede.Uof.WKF.Utility.TaskUtilityUCO taskUCO = new Ede.Uof.WKF.Utility.TaskUtilityUCO();
+                result =taskUCO.WebService_CreateTask(form.ConvertToFormInfoXml());
+
+
+                XElement resultXE = XElement.Parse(result);
+                string msg = "";
+                if (resultXE.Element("Status").Value == "1")
+                {
+                    string formNBR = resultXE.Element("FormNumber").Value;
+                    msg = $"起單成功{formNBR}";
+
+                    Logger.Write("SystexInfo", msg);
+                }
+                else
+                {
+                    string type = resultXE.Element("Exception").Element("Type").Value;
+                    msg = $"起單失敗\r\n，失敗原因{type}，表單XML:{form.ConvertToFormInfoXml()}";
+
+                    Logger.Write("SystexInfo",msg);
+                    throw new Exception(msg);
+                
+                }
+
+
+            }
+            
             return "";
         }
 
